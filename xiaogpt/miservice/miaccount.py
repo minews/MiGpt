2f0cf3a6203ceb8b2683301f757603d6dce0b895
yinming <xxxx@x.com>
2026-02-25 17:52:50 +0800

    feat: 临时关闭ssl验证

--- xiaogpt/miservice/miaccount.py
+++ xiaogpt/miservice/miaccount.py
@@ -131,6 +131,7 @@ class MiAccount:
         async with self.session.get(url,
             cookies=cookie_dict,
             headers=headers,
+            ssl = False
         ) as r:
             raw = await r.read()
             resp = json.loads(raw[11:])
@@ -157,7 +158,8 @@ class MiAccount:
         async with self.session.get(url,
             data=data,
             cookies=cookies,
-            headers=headers
+            headers=headers,
+            ssl = False
         ) as r:
             raw = await r.read()
             resp = json.loads(raw[11:])
